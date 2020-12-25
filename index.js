bbf37b8512104b52370920ff7ddcf224f1b9908f
chan <dauni0419@gmail.com>
2020-12-25 12:26:20 +0900

    and start....

---
+++ index.js
@@ -0,0 +1,11 @@
+//짚을 설명들
+//server:
+//접속을 받아주고 접속을 listen하고 있는 무언가
+//npm:
+//npm은 node.js에서 package를 사용하기 위한 도구다 일단 npm을 사용할려면 npm init을 한뒤
+//여러 정보를 입력하고 깔고 싶은 package를 npm install package명 이렇게 install하면된다
+//핵심 포인트
+//express:
+//express는 node.js로 서버를 간단한 코드 몇줄로 열수있게 도와주는 framework이다
+
+//express 시작
